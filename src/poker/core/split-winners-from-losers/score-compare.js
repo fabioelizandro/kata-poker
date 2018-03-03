3fb644f97c2cf7b5da5ab7a7e0d662fe2dcbf3fc
Fabio Elizandro <fabio@elizandro.com.br>
2018-03-03 23:13:33 +1100

    Fix score compare.

--- src/poker/core/split-winners-from-losers/score-compare.js
+++ src/poker/core/split-winners-from-losers/score-compare.js
@@ -1,12 +1,9 @@
 module.exports = (scoreA, scoreB) => {
-  const orderedScoreA = scoreA.sort((a, b) => b - a);
-  const orderedScoreB = scoreB.sort((a, b) => b - a);
-
   const findHighest = (index) => {
-    const highestA = orderedScoreA[index];
-    const highestB = orderedScoreB[index];
+    const highestA = scoreA[index];
+    const highestB = scoreB[index];
     const result = highestB - highestA;
-    const hasMoreToCompare = index < orderedScoreA.length - 1;
+    const hasMoreToCompare = index < scoreA.length - 1;
 
     if (result === 0 && hasMoreToCompare) {
       return findHighest(index + 1);
--- src/poker/core/split-winners-from-losers/score-compare.spec.js
+++ src/poker/core/split-winners-from-losers/score-compare.spec.js
@@ -2,44 +2,37 @@ const scoreCompare = require('./score-compare');
 
 describe('rank compare', () => {
   it('returns positive when the highest A score is less than B', () => {
-    const scoreA = [9, 8, 7, 6, 5];
-    const scoreB = [10, 8, 7, 6, 5];
+    const scoreA = [9];
+    const scoreB = [10];
 
     expect(scoreCompare(scoreA, scoreB)).toEqual(1);
   });
 
   it('returns 0 when the highest A score is equal to B', () => {
-    const scoreA = [9, 8, 7, 6, 5];
-    const scoreB = [9, 8, 7, 6, 5];
+    const scoreA = [9];
+    const scoreB = [9];
 
     expect(scoreCompare(scoreA, scoreB)).toEqual(0);
   });
 
   it('returns negative when the highest A score is greater than B', () => {
-    const scoreA = [10, 8, 7, 6, 5];
-    const scoreB = [9, 8, 7, 6, 5];
+    const scoreA = [10];
+    const scoreB = [9];
 
     expect(scoreCompare(scoreA, scoreB)).toEqual(-1);
   });
 
-  it('finds the hightest score when is not ordered by the highest', () => {
-    const scoreA = [8, 7, 10, 6, 5];
-    const scoreB = [8, 7, 6, 9, 5];
-
-    expect(scoreCompare(scoreA, scoreB)).toEqual(-1);
-  });
-
-  it('falls to the next highest score and the highest ties', () => {
-    const scoreA = [9, 8, 7, 6, 5];
-    const scoreB = [9, 8, 7, 5, 4];
+  it('breakes tie at first difference', () => {
+    const scoreA = [9, 8, 5];
+    const scoreB = [9, 8, 4];
     expect(scoreCompare(scoreA, scoreB)).toEqual(-1);
 
-    const scoreC = [9, 8, 7, 6, 4];
-    const scoreD = [9, 8, 7, 6, 5];
+    const scoreC = [9, 6, 14];
+    const scoreD = [9, 7, 13];
     expect(scoreCompare(scoreC, scoreD)).toEqual(1);
 
-    const scoreE = [9, 8, 7, 4, 3];
-    const scoreF = [9, 8, 6, 4, 3];
+    const scoreE = [2, 3, 7, 5];
+    const scoreF = [2, 3, 6, 14];
     expect(scoreCompare(scoreE, scoreF)).toEqual(-1);
   });
 });
