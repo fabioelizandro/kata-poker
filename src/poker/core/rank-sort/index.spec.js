d559db0c536bc6e694c59971a9530eca7e0fab13
Fabio Elizandro <fabio@elizandro.com.br>
2018-03-03 16:28:51 +1100

    Fix rank sort to support score as array.

--- src/poker/core/rank-sort/index.spec.js
+++ src/poker/core/rank-sort/index.spec.js
@@ -2,11 +2,11 @@ const rankSort = require('./');
 
 describe('rank sort', () => {
   it('sorts ranks by level and score', () => {
-    const rankA = { level: 3, score: 6 };
-    const rankB = { level: 2, score: 6 };
-    const rankC = { level: 2, score: 5 };
-    const rankD = { level: 1, score: 6 };
-    const rankE = { level: 1, score: 5 };
+    const rankA = { level: 3, score: [6] };
+    const rankB = { level: 2, score: [6] };
+    const rankC = { level: 2, score: [5] };
+    const rankD = { level: 1, score: [6] };
+    const rankE = { level: 1, score: [5] };
 
     const shuffled = [rankE, rankB, rankA, rankD, rankC];
 
--- src/poker/core/rank-sort/rank-compare.js
+++ src/poker/core/rank-sort/rank-compare.js
@@ -1,3 +1,5 @@
+const scoreCompare = require('./score-compare');
+
 module.exports = (rankA, rankB) => {
-  return (rankB.level - rankA.level) || (rankB.score - rankA.score);
+  return (rankB.level - rankA.level) || scoreCompare(rankA.score, rankB.score);
 };
