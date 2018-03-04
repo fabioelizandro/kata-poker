e0300eb7a007005ed121eafe4c91395342c7edb3
Fabio Elizandro <fabio@elizandro.com.br>
2018-03-04 14:49:32 +1100

    Add suggested test case.

--- src/poker/index.spec.js
+++ src/poker/index.spec.js
@@ -1,17 +1,21 @@
 const poker = require('./');
 
 describe('poker', () => {
-  it('returns the winner', () => {
-    const sample = `
-    Player1: 2H 3D 5S 9C KD  Player2: 2C 3H 4S 8C AH
-    Player1: 2H 3D 5S 9C AD  Player2: 2C 3H 4S 8C KH
-    Player1: 2H 3D 5S 9C KD  Player2: 2D 3H 5C 9S KH
-    `
+  describe('kata suggested test case', () => {
+    it('gives the same output', () => {
+      const sample = `
+      Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH
+      Black: 2H 4S 4C 2D 4H  White: 2S 8S AS QS 3S
+      Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C KH
+      Black: 2H 3D 5S 9C KD  White: 2D 3H 5C 9S KH
+      `
 
-    expect(poker(sample)).toEqual(
-      'Winner: Player2 - with high-card\n'+
-      'Winner: Player1 - with high-card\n'+
-      'Tie.'
-    )
+      expect(poker(sample)).toEqual(
+        'Winner: White - with high-card\n'+
+        'Winner: Black - with full-house\n'+
+        'Winner: Black - with high-card\n'+
+        'Tie.'
+      )
+    });
   });
 });
