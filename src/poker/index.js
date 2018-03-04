23ed525b888aed25d7fd5994be79cf17c2e25dbd
Fabio Elizandro <fabio@elizandro.com.br>
2018-03-04 15:17:44 +1100

    Remove unused code.

--- src/poker/index.js
+++ src/poker/index.js
@@ -4,7 +4,6 @@ const serializer = require('./serializer');
 
 module.exports = (match) => {
   const games = parser(match);
-  const result = games.map(core);
   const serialized = games.map(core).map(result => serializer(result.winners));
 
   return serialized.join('\n');
